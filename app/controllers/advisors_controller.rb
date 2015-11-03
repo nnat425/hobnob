814aa9f392009e57711ccc0769d8a165e32188b0
Jenny Lee <leejenny0403@gmail.com>
2015-11-03 17:12:40 -0500

    add settings method to advisors controller

--- app/controllers/advisors_controller.rb
+++ app/controllers/advisors_controller.rb
@@ -8,11 +8,14 @@ class AdvisorsController < ApplicationController
     advisor = Advisor.new(advisor_params)
     if advisor.save && advisor.valid?
       session[:user_id] = advisor.id
-      redirect_to advisor_path(advisor)
+      redirect_to "/advisors/#{advisor.id}/settings"
     else
       flash[:error] = advisor.errors.full_messages
       redirect_to new_advisor_path
     end
+  end
+
+  def settings
   end
 
   def show
