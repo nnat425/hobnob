af02b05145aaa2305e32e722c082cf532be4a19b
Navraj Nat <nnat425@gmail.com>
2015-11-03 15:48:39 -0500

    Add password confirmation validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,13 @@ has_many :appointments
 validates :first_name,:last_name, :presence => true
 
 validates :email, uniqueness: true
-validates :email, confirmation: true
 validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+validates :password, confirmation: true
+
+
+has_secure_password
+
 
 
 end
