a66601f77f9035146f2cc1886697f1d5e0a7d11f
Jenny Lee <leejenny0403@gmail.com>
2015-11-03 16:00:23 -0500

    add create method to advisors controller

--- app/controllers/advisors_controller.rb
+++ app/controllers/advisors_controller.rb
@@ -4,4 +4,16 @@ class AdvisorsController < ApplicationController
     @advisor = Advisor.new
   end
 
+  def create
+    advisor = Advisor.new(advisor_params)
+    if advisor.save && advisor.valid?
+      session[:user_id] = advisor.id
+      redirect_to advisor_path(advisor)
+    else
+      @errors = user.errors.full_messages
+      render "new"
+    end
+  end
+
+
 end
