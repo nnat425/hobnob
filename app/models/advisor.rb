74ee08a0834c50b1c873c805c3f24e07a57e09e4
Jenny Lee <leejenny0403@gmail.com>
2015-11-03 15:09:07 -0500

    add advisor model with some validations

---
+++ app/models/advisor.rb
@@ -0,0 +1,7 @@
+class Advisor < ActiveRecord::Base
+  has_many :categories
+  validates :firstname, :lastname, :email, :presence => true
+  validates :email, uniqueness: true, confirmation: true
+  validates :email_confirmation, presence: true
+  has_secure_password
+end
