4754c9bfd627e896296f6088b9663d1c2e42caa7
Jenny Lee <leejenny0403@gmail.com>
2015-11-03 15:31:34 -0500

    add bcrypt to gemfile

--- app/models/advisor.rb
+++ app/models/advisor.rb
@@ -3,6 +3,6 @@ class Advisor < ActiveRecord::Base
   validates :firstname, :lastname, :email, :presence => true
   validates :email, uniqueness: true, confirmation: true
   validates :email_confirmation, presence: true
-  validates :password, length: { 6..20 }
+
   has_secure_password
 end
