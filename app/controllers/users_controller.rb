4fb1b1fd52372489b3dcd24d7e20aab22fd84de0
Navraj Nat <nnat425@gmail.com>
2015-11-04 11:35:50 -0500

    Add session when user creates account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      session[:user_id] = user.id
       render file: "thanks"
     else
       flash[:errors] = user.errors.full_messages
