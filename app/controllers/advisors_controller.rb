40e3783ab78afdc67f73a2dba7c536eb208c7c10
Jenny Lee <leejenny0403@gmail.com>
2015-11-03 22:40:59 -0500

    add edit profile view page for advisors

--- app/controllers/advisors_controller.rb
+++ app/controllers/advisors_controller.rb
@@ -8,7 +8,7 @@ class AdvisorsController < ApplicationController
     advisor = Advisor.new(advisor_params)
     if advisor.save && advisor.valid?
       session[:user_id] = advisor.id
-      redirect_to advisor_settings_path(advisor)
+      redirect_to edit_advisor_path(advisor)
     else
       flash[:error] = advisor.errors.full_messages
       redirect_to new_advisor_path
