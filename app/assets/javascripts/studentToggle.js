a8569d92328c3fa96efb49a17f17829374585536
Navraj Nat <nnat425@gmail.com>
2015-11-04 10:51:31 -0500

    Show and Hide program and graduation date functioning

--- app/assets/javascripts/studentToggle.js
+++ app/assets/javascripts/studentToggle.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
-  if($('#user_student_false').prop("checked",true)){
-    $('#user_program').hide();
-    $('#user_graduation_date').hide();
-  }
+  $( "#user_student_true" ).on('click',function() {
+    $( "#user_program" ).show();
+    $("#user_graduation_date").show();
+  });
+
+  $( "#user_student_false" ).on('click',function() {
+    $( "#user_program" ).hide();
+    $("#user_graduation_date").hide();
+  });
+
 });
+
