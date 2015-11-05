be3f215973588288b935ead035ef22ac476835e6
Navraj Nat <nnat425@gmail.com>
2015-11-04 23:21:06 -0500

    Javascript code functioning correctly on user sign up page

--- app/assets/javascripts/studentToggle.js
+++ app/assets/javascripts/studentToggle.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
+  $( "#user_program" ).hide();
+  $("#grad_date").hide();
   $( "#user_student_true" ).on('click',function() {
     $( "#user_program" ).show();
-    $("#user_graduation_date").show();
+    $("#grad_date").show();
   });
-
   $( "#user_student_false" ).on('click',function() {
-    $( "#user_program" ).hide();
-    $("#user_graduation_date").hide();
-  });
+   $( "#user_program" ).hide();
+   $("#grad_date").hide();
+ });
 
 });
 
