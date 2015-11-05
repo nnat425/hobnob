4e53b78f3d56a59c8dc7c0ededb8b1d921a618de
Jenny Lee <leejenny0403@gmail.com>
2015-11-05 12:18:15 -0500

    add email confirmation for user signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ has_many :carts
 has_many :appointments
 
 
-validates :first_name,:last_name, :presence => true
-
+validates :first_name,:last_name, :email, :presence => true
 validates :email, uniqueness: true
 validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+validates_confirmation_of :email, :message => "Should match confirmation"
 
-
+validates_confirmation_of :password
+validates_presence_of :password_confirmation
 
 has_secure_password
 
