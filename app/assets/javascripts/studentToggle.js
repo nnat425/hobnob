ee95f1667de3457444f278f3d29a0d9c93f066d0
Navraj Nat <nnat425@gmail.com>
2015-11-05 09:59:08 -0500

    Add paragraph in user signup form

--- app/assets/javascripts/studentToggle.js
+++ app/assets/javascripts/studentToggle.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
   $( "#user_program" ).hide();
   $("#grad_date").hide();
+  $("#user_school").hide();
+
   $( "#user_student_true" ).on('click',function() {
     $( "#user_program" ).show();
     $("#grad_date").show();
+    $("#user_school").show();
   });
+
   $( "#user_student_false" ).on('click',function() {
    $( "#user_program" ).hide();
    $("#grad_date").hide();
+    $("#user_school").hide();
+
  });
 
 });
