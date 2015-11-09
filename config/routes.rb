fbf6b281faee226f770e72b0a677558e19a18083
Navraj Nat <nnat425@gmail.com>
2015-11-09 13:18:48 -0500

    Add about route in config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'about' => 'welcome#about'
   resources :advisors
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
 
-  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
