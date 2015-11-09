7af93fdd26825d45473a9485e6944d9d369bd3b8
Jenny Lee <leejenny0403@gmail.com>
2015-11-08 20:56:26 -0500

    add file name validation for advisor avatar in advisor model

--- app/models/advisor.rb
+++ app/models/advisor.rb
@@ -10,6 +10,7 @@ class Advisor < ActiveRecord::Base
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
 
   has_secure_password
 
