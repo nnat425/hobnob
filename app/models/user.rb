ededed7ea6663392ea380190abe9e8a3df76516f
Jenny Lee <leejenny0403@gmail.com>
2015-11-08 20:56:01 -0500

    add file name validation for user avatar in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ validates_presence_of :email_confirmation
 
 has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
 
 has_secure_password
 
