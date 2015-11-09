809d1f5e8d2df7168a11e77b0b94a9d64d09807b
Jenny Lee <leejenny0403@gmail.com>
2015-11-08 20:44:51 -0500

    add upload photo form to advisor edit page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103195435) do
+ActiveRecord::Schema.define(version: 20151109012520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,19 +34,27 @@ ActiveRecord::Schema.define(version: 20151103195435) do
     t.string   "interesting_facts"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name", null: false
-    t.string   "last_name",                      null: false
-    t.string   "email",                           null: false
-    t.string   "password_digest",                 null: false
-    t.boolean  "student",         default: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
+    t.string   "email",                               null: false
+    t.string   "password_digest",                     null: false
+    t.boolean  "student",             default: false
     t.string   "school"
     t.string   "program"
     t.integer  "graduation_date"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
