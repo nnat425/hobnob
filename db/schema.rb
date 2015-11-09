d63551395ad80ce35b6a162618bec0b12f39ea0d
Navraj Nat <nnat425@gmail.com>
2015-11-08 19:45:56 -0500

    Create category migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109012520) do
+ActiveRecord::Schema.define(version: 20151109003342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,12 +40,27 @@ ActiveRecord::Schema.define(version: 20151109012520) do
     t.datetime "avatar_updated_at"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.integer  "advisor_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
+<<<<<<< HEAD
     t.string   "first_name",                          null: false
     t.string   "last_name",                           null: false
     t.string   "email",                               null: false
     t.string   "password_digest",                     null: false
     t.boolean  "student",             default: false
+=======
+    t.string   "first_name",                      null: false
+    t.string   "last_name",                       null: false
+    t.string   "email",                           null: false
+    t.string   "password_digest",                 null: false
+    t.boolean  "student",         default: false
+>>>>>>> Create category migration
     t.string   "school"
     t.string   "program"
     t.integer  "graduation_date"
