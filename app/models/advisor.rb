2b46e0723601b94b7d9849c077980c2ae4b16cbf
Jenny Lee <leejenny0403@gmail.com>
2015-11-08 20:24:17 -0500

    add image validations to advisor model

--- app/models/advisor.rb
+++ app/models/advisor.rb
@@ -4,9 +4,12 @@ class Advisor < ActiveRecord::Base
   validates :email, :presence => true
   validates :email, uniqueness: true
 
-validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates_confirmation_of :email, :message => "Should match confirmation"
   validates_presence_of :email_confirmation
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   has_secure_password
 
