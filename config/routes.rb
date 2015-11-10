d32cb82ade57b25d42fbe6358461536b4b99ee03
Navraj Nat <nnat425@gmail.com>
2015-11-09 23:52:16 -0500

    Add session route in config file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :advisors
   resources :users
 
+  get 'login' => 'sessions#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
