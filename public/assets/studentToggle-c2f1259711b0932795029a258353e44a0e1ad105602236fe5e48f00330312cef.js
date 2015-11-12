dc76d7ab82e5b3c026bbab1645d05bd5f2f9908a
Navraj Nat <nnat425@gmail.com>
2015-11-12 18:28:07 -0500

    precompile assets

---
+++ public/assets/studentToggle-c2f1259711b0932795029a258353e44a0e1ad105602236fe5e48f00330312cef.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#user_program").hide(),$("#grad_date").hide(),$("#user_school").hide(),$("#yes-no").on("click",function(){$("#user_program").slideToggle(),$("#grad_date").slideToggle(),$("#user_school").slideToggle()})});
