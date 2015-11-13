6af5be4f40aba6f7ea3d5e9ede2886c3084e72ff
Navraj Nat <nnat425@gmail.com>
2015-11-12 23:08:30 -0500

    Add correct logic to session controller. Method create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,32 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: session_params[:email])
-    advisor = advisor.find_by(email: session_params[:email])
-    if user.try(:authenticate, session_params[:password]) || advisor.try(:authenticate, session_params[:password])
+    advisor = Advisor.find_by(email: session_params[:email])
+    if user
+      if user.try(:authenticate, session_params[:password])
+        session[:user_or_advisor_id] = user.id
+        redirect_to root_path
+      else
+        flash[:login_fail] = "Please input the correct username/password!"
+        redirect_to login_path
+      end
+    elsif advisor.try(:authenticate, session_params[:password])
+      session[:user_or_advisor_id] = advisor.id
+      redirect_to root_path
     else
       flash[:login_fail] = "Please input the correct username/password!"
       redirect_to login_path
     end
+  end
+
+  def delete
+    session[:user_id] = nil
+    redirect_to login_path
+  end
+
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 
 end
