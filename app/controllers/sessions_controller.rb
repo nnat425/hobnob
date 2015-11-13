a07b644e055ae17bd5165fc1997487b594714cc0
Navraj Nat <nnat425@gmail.com>
2015-11-12 21:59:43 -0500

    Add create method to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,16 @@
 class SessionsController < ApplicationController
 
-def new
-end
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+    advisor = advisor.find_by(email: session_params[:email])
+    if user.try(:authenticate, session_params[:password]) || advisor.try(:authenticate, session_params[:password])
+    else
+      flash[:login_fail] = "Please input the correct username/password!"
+      redirect_to login_path
+    end
+  end
 
 end
