9a07efdafb830b9227ac5900956a0eaad4be2d83
Navraj Nat <nnat425@gmail.com>
2015-11-16 12:01:20 -0500

    Create carts migration

--- db/migrate/20151116165234_create_potential_appointments.rb
+++ db/migrate/20151116165234_create_potential_appointments.rb
@@ -2,7 +2,7 @@ class CreatePotentialAppointments < ActiveRecord::Migration
   def change
     create_table :potential_appointments do |t|
       t.references :advisor, null: false
-      t.references :cart
+      t.references :cart, null: false
       t.time :start_time , null: false
       t.time :end_time, null: false
       t.date :date, null: false
