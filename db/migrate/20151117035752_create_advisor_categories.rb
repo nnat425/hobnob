f6db0272ebbd48911ccf8c88c2b874d1ebceb795
Jenny Lee <leejenny0403@gmail.com>
2015-11-16 22:59:34 -0500

    add references for advisor and category for advisor category table

--- db/migrate/20151117035752_create_advisor_categories.rb
+++ db/migrate/20151117035752_create_advisor_categories.rb
@@ -1,6 +1,8 @@
 class CreateAdvisorCategories < ActiveRecord::Migration
   def change
     create_table :advisor_categories do |t|
+      t.references :advisor, index: true, foreign_key: true
+      t.references :category, index: true, foreign_key: true
     end
   end
 end
