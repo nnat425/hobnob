ba7e01abf1018b67acc63331bb4d69d045bad05a
Jenny Lee <leejenny0403@gmail.com>
2015-11-16 23:05:00 -0500

    change advisorcategories table to advisors categories migration

--- db/migrate/20151117035752_create_advisors_categories.rb
+++ db/migrate/20151117035752_create_advisors_categories.rb
@@ -1,6 +1,6 @@
-class CreateAdvisorCategories < ActiveRecord::Migration
+class CreateAdvisorsCategories < ActiveRecord::Migration
   def change
-    create_table :advisor_categories do |t|
+    create_table :advisors_categories do |t|
       t.references :advisor, index: true, foreign_key: true
       t.references :category, index: true, foreign_key: true
     end
