22a86ab6ceadb23f202d7b6ae6c9a901d38c6e8b
Navraj Nat <nnat425@gmail.com>
2015-11-16 22:10:15 -0500

    Appointment displays on calendar

--- app/controllers/potential_appointments_controller.rb
+++ app/controllers/potential_appointments_controller.rb
@@ -9,13 +9,11 @@ class PotentialAppointmentsController < ApplicationController
 
   def create
 
-    date = Date.new(params[:potential_appointment].values[0].to_i, params[:potential_appointment].values[1].to_i,params[:potential_appointment].values[2].to_i)
+    start_time = Time.new(params[:potential_appointment].values[0].to_i, params[:potential_appointment].values[1].to_i,params[:potential_appointment].values[2].to_i,params[:potential_appointment].values[6].to_i,params[:potential_appointment].values[7].to_i).to_s
 
-    start_time = Time.new(params[:potential_appointment].values[3].to_i, params[:potential_appointment].values[4].to_i,params[:potential_appointment].values[5].to_i,params[:potential_appointment].values[6].to_i,params[:potential_appointment].values[7].to_i).to_s
+    end_time = Time.new(params[:potential_appointment].values[0].to_i, params[:potential_appointment].values[1].to_i,params[:potential_appointment].values[2].to_i,params[:potential_appointment].values[11].to_i,params[:potential_appointment].values[12].to_i).to_s
 
-    end_time = Time.new(params[:potential_appointment].values[8].to_i, params[:potential_appointment].values[9].to_i,params[:potential_appointment].values[10].to_i,params[:potential_appointment].values[11].to_i,params[:potential_appointment].values[12].to_i).to_s
-
-    potential_appointment = current_advisor.potential_appointments.create(start_time: start_time, end_time: end_time, date: date)
+    potential_appointment = current_advisor.potential_appointments.create(start_time: start_time, end_time: end_time)
 
     # if potential_appointment.start_time != nil
       redirect_to root_path
--- db/migrate/20151116165234_create_potential_appointments.rb
+++ db/migrate/20151116165234_create_potential_appointments.rb
@@ -3,10 +3,8 @@ class CreatePotentialAppointments < ActiveRecord::Migration
     create_table :potential_appointments do |t|
       t.references :advisor, null: false
       t.references :cart
-
       t.datetime :start_time , null: false
       t.datetime :end_time, null: false
-      t.date :date, null: false
       t.boolean :booked_status?, default: false
       t.timestamps null: false
     end
