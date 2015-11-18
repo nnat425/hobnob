cf64416dd74fd21991303cf7fa8c204d36bc873f
Jenny Lee <leejenny0403@gmail.com>
2015-11-18 09:40:15 -0500

    add route for filtering advisor by category

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
+  get 'category/:id/' => 'category#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
