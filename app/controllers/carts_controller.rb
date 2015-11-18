ea27cb608aaded1263986fbe8137ba09d01c7992
Navraj Nat <nnat425@gmail.com>
2015-11-18 00:59:57 -0500

    Cart show page template working

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,8 +3,8 @@ class CartsController < ApplicationController
 def show
 @cart = Cart.find_by(id: params[:id])
 @advisors = []
-@cart.potential_appointments.each do |advisor_id|
-  @advisors << Advisor.find_by(id: advisor_id)
+@cart.potential_appointments.each do |appointment|
+  @advisors << Advisor.find_by(id:appointment.advisor_id)
 end
 end
 
