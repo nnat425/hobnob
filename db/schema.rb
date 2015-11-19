46453d37114f24aa70719c6327539dd5df3250af
Navraj Nat <nnat425@gmail.com>
2015-11-19 17:45:00 -0500

    Money is getting sent to paypal account

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119194601) do
+ActiveRecord::Schema.define(version: 20151119223606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20151119194601) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.boolean  "checked_out?", default: false
+    t.time     "purchased_at"
   end
 
   create_table "carts_potential_appointments", force: :cascade do |t|
