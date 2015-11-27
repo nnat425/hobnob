c5c02833ae561117b795c3fcc786a20580056d94
Navraj Nat <nnat425@gmail.com>
2015-11-27 16:18:55 -0500

    Add conditionals to create

--- app/controllers/potential_appointments_controller.rb
+++ app/controllers/potential_appointments_controller.rb
@@ -10,6 +10,7 @@ class PotentialAppointmentsController < ApplicationController
   def create
 
     date = params[:appointment_date][:date].split('-')
+    if date
 
     start_time = Time.new(date[0],date[1],date[2],params[:potential_appointment].values[4].to_i,params[:potential_appointment].values[5].to_i).to_s
 
@@ -18,6 +19,9 @@ class PotentialAppointmentsController < ApplicationController
     potential_appointment = current_advisor.potential_appointments.create(title: params[:potential_appointment][:title],start_time: start_time, end_time: end_time)
 
     redirect_to root_path
+  else
+    redirect_to new_potential_appointment
+    flash[:pick_date] = "Please pick a date/Enter a title"
   end
 
   def update
