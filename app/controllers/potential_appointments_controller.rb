a08f120dca9c7a64cf697177fdadcb5a74a01189
Navraj Nat <nnat425@gmail.com>
2015-11-27 16:55:06 -0500

    Need to add flash error messages

--- app/controllers/potential_appointments_controller.rb
+++ app/controllers/potential_appointments_controller.rb
@@ -3,25 +3,26 @@ class PotentialAppointmentsController < ApplicationController
   def new
     @potential_appointment = PotentialAppointment.new
 
-    @all_appointments =
-    PotentialAppointment.all
+    current_advisor = Advisor.find_by(id: session[:user_or_advisor_id])
+    @advisor_appointments = current_advisor.potential_appointments
   end
 
   def create
 
     date = params[:appointment_date][:date].split('-')
-    if date
+    if date[0] != nil
 
-    start_time = Time.new(date[0],date[1],date[2],params[:potential_appointment].values[4].to_i,params[:potential_appointment].values[5].to_i).to_s
+      start_time = Time.new(date[0],date[1],date[2],params[:potential_appointment].values[4].to_i,params[:potential_appointment].values[5].to_i).to_s
 
-    end_time = Time.new(date[0],date[1],date[2],params[:potential_appointment].values[9].to_i,params[:potential_appointment].values[10].to_i).to_s
+      end_time = Time.new(date[0],date[1],date[2],params[:potential_appointment].values[9].to_i,params[:potential_appointment].values[10].to_i).to_s
 
-    potential_appointment = current_advisor.potential_appointments.create(title: params[:potential_appointment][:title],start_time: start_time, end_time: end_time)
+      potential_appointment = current_advisor.potential_appointments.create(title: params[:potential_appointment][:title],start_time: start_time, end_time: end_time)
 
-    redirect_to root_path
-  else
-    redirect_to new_potential_appointment
-    flash[:pick_date] = "Please pick a date/Enter a title"
+      redirect_to new_potential_appointment_path
+    else
+      redirect_to new_potential_appointment_path
+      flash[:pick_date_or_enter_title] = "Please pick a date/Enter a title"
+    end
   end
 
   def update
