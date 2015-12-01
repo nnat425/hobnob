2655b0c17cd1574717b6287f461021e5871b444e
Navraj Nat <nnat425@gmail.com>
2015-12-01 14:02:54 -0500

    update price_in_cents method

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,7 +8,7 @@ class Order < ActiveRecord::Base
 
   validate :validate_card, :on => :create
 
-def purchase
+  def purchase
     response = GATEWAY.purchase(price_in_cents, credit_card, :ip => ip_address)
     transactions.create!(:action => "purchase", :amount => price_in_cents, :response => response)
     cart.update_attribute(:purchased_at, Time.now) if response.success?
@@ -16,32 +16,36 @@ def purchase
   end
 
   def price_in_cents
+    if cart.user.student == true
+      return self.cart.total_price_student
+    else
     # (cart.total_price*100).round
-    return 100
+    return  self.cart.total_price_regular * 100
   end
+end
 
 
 
 
 private
 
-  def validate_card
-    unless credit_card.valid?
-      credit_card.errors.full_messages.each do |message|
-        errors[:base] << message
-      end
+def validate_card
+  unless credit_card.valid?
+    credit_card.errors.full_messages.each do |message|
+      errors[:base] << message
     end
   end
+end
 
-  def credit_card
-    @credit_card ||= ActiveMerchant::Billing::CreditCard.new(
-      :type               => card_type,
-      :number             => card_number,
-      :verification_value => card_verification,
-      :month              => card_expires_on.month,
-      :year               => card_expires_on.year,
-      :first_name         => first_name,
-      :last_name          => last_name
-      )
-  end
+def credit_card
+  @credit_card ||= ActiveMerchant::Billing::CreditCard.new(
+    :type               => card_type,
+    :number             => card_number,
+    :verification_value => card_verification,
+    :month              => card_expires_on.month,
+    :year               => card_expires_on.year,
+    :first_name         => first_name,
+    :last_name          => last_name
+    )
+end
 end
