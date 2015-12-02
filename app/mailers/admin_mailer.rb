f2c212877503a400c01136c9e11a4b3ac4c7a2ef
Navraj Nat <nnat425@gmail.com>
2015-12-02 01:05:02 -0500

    Fix string interpolation bug

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,7 +4,7 @@ def confirmation_email(user, appointment)
     @user = user
     @appointment  = appointment
     @advisor = Advisor.find_by(id: appointment.advisor_id)
-    mail(to: 'nnat425@gmail.com', subject: 'Hobnob:Appointment #' + @appointment.id.to_s +" has been booked!")
+    mail(to: 'nnat425@gmail.com', subject: 'Hobnob:Appointment # #{@appointment.id} has been booked!')
   end
 
 end
