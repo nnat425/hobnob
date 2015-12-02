67d20481792d83dd1e37cca063b3f6aad160e911
Navraj Nat <nnat425@gmail.com>
2015-12-02 14:55:04 -0500

    Appointment gets updated appropriately

--- app/controllers/potential_appointments_controller.rb
+++ app/controllers/potential_appointments_controller.rb
@@ -20,25 +20,46 @@ class PotentialAppointmentsController < ApplicationController
 
       redirect_to new_potential_appointment_path
     else
-       flash[:pick_date_or_enter_title] = "Please pick a date/Enter a title"
+     flash[:pick_date_or_enter_title] = "Please pick a date/Enter a title"
 
-      redirect_to new_potential_appointment_path
+     redirect_to new_potential_appointment_path
 
-    end
-  end
+   end
+ end
 
-  def update
-  end
-
-  def edit
+ def edit
   @potential_appointment = PotentialAppointment.find_by(id: params[:id])
   @advisor_appointments = current_advisor.potential_appointments
-  end
+end
 
-  private
 
-  def appointment_params
-    params.require(:potential_appointment).permit(:start_time,:end_time,:date)
-  end
+def update
+  potential_appointment = PotentialAppointment.find_by(id: params[:id])
+
+  date = params[:appointment_date][:date].split('-')
+  if date[0] != nil && params[:potential_appointment][:title] != ""
+
+    start_time = Time.new(date[0],date[1],date[2],params[:potential_appointment].values[4].to_i,params[:potential_appointment].values[5].to_i).to_s
+
+    end_time = Time.new(date[0],date[1],date[2],params[:potential_appointment].values[9].to_i,params[:potential_appointment].values[10].to_i).to_s
+
+    potential_appointment.update(title: params[:potential_appointment][:title],start_time: start_time, end_time: end_time)
+
+    redirect_to advisor_path(current_advisor)
+
+  else
+   flash[:pick_date_or_enter_title] = "Please pick a date/Enter a title"
+   redirect_to edit_potenital_appointment_path(potential_appointment)
+
+ end
+end
+
+
+
+private
+
+def appointment_params
+  params.require(:potential_appointment).permit(:start_time,:end_time,:title)
+end
 
 end
