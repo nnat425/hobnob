be561a949b6a8c6491c8266574bc75f825e5e373
Jenny Lee <leejenny0403@gmail.com>
2015-12-01 19:17:54 -0500

    add passing test for user redirected to profile page when signup with valid info

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -19,4 +19,18 @@ feature "Signing up" do
       end
     end
   end
+
+  describe "with valid information" do
+    scenario "redirects to profile page" do
+      visit new_user_path
+      fill_in "First Name", :with => user_attr[:first_name]
+      fill_in "Last Name", :with => user_attr[:last_name]
+      fill_in "Email", :with => user_attr[:email]
+      fill_in "Confirm Email", :with => user_attr[:email]
+      fill_in "Password", :with => user_attr[:password]
+      fill_in "Password Confirmation", :with => user_attr[:password]
+      click_button "Register"
+      expect(page).to have_content "Thanks for signing up!"
+    end
+  end
 end
