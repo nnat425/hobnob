7dc22c2e2cc8bf71885322bb8beae0e3061e2f08
Jenny Lee <leejenny0403@gmail.com>
2015-12-02 21:24:28 -0500

    remove categories advisors migration table

--- db/migrate/20151117035752_create_advisors_categories.rb
+++
@@ -1,8 +0,0 @@
-class CreateAdvisorsCategories < ActiveRecord::Migration
-  def change
-    create_table :advisors_categories do |t|
-      t.references :advisor, index: true, foreign_key: true
-      t.references :category, index: true, foreign_key: true
-    end
-  end
-end
