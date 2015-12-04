76ca593df153fdc69b7c2dd19520cfb9d7ad0a66
Navraj Nat <nnat425@gmail.com>
2015-12-03 22:16:53 -0500

    Need to fix mobile view for user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,31 @@
 class User < ActiveRecord::Base
 
-has_many :carts
-has_many :potential_appointments
+  has_many :carts
+  has_many :potential_appointments
 
 
-validates :first_name,:last_name, :email, :presence => true
-validates :email, uniqueness: true
-validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-validates_confirmation_of :email, :message => "should match email"
-validates_presence_of :email_confirmation
+  validates :first_name,:last_name, :email, :presence => true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates_confirmation_of :email, :message => "should match email"
+  validates_presence_of :email_confirmation
 
-has_secure_password
+  has_secure_password
 
-after_save :create_cart
+  after_save :create_cart
 
-def create_cart
-  self.carts.create
-end
+  def create_cart
+    self.carts.create
+  end
 
+  def booked_appointments
+    booked_appointment = []
+    booked_advisor = []
+    self.carts.each do |cart|
+      booked_appointment.push(cart.potential_appointments.where(booked_status?: true ))
+    end
+    return booked_appointment.flatten
+  end
 
 
 end
