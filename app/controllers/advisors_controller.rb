aba2078133f53f8e27c4bc3e869b19da32a80bdf
Jenny Lee <leejenny0403@gmail.com>
2015-12-05 11:17:29 -0500

    revert algorithm to have find or create by category

--- app/controllers/advisors_controller.rb
+++ app/controllers/advisors_controller.rb
@@ -43,8 +43,8 @@ class AdvisorsController < ApplicationController
     advisor = Advisor.find_by(id: params[:id])
     if advisor_params && params[:category]
       advisor.categories = []
-      params[:category][:name].each do |category|
-        advisor.categories << category
+      params[:category][:name].each do |category_name|
+        advisor.categories << Category.find_or_create_by(name: category_name)
       end
       advisor.update(advisor_params)
       advisor.join_companies(params[:companies])
