bb326f49f4ddddf385aac708c656e66948abfac4
Jenny Lee <leejenny0403@gmail.com>
2015-12-05 11:28:28 -0500

    create categories migration table

--- db/migrate/20151205162253_create_advisors_categories.rb
+++ db/migrate/20151205162253_create_advisors_categories.rb
@@ -3,6 +3,8 @@ class CreateAdvisorsCategories < ActiveRecord::Migration
     create_table :advisors_categories do |t|
       t.references :advisor, index: true, foreign_key: true
       t.references :category, index: true, foreign_key: true
+
+      t.timestamps null: false
     end
   end
 end
