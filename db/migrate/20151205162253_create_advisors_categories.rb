aebad6e02adcf8447da71aefb9e4818f6c69fffd
Jenny Lee <leejenny0403@gmail.com>
2015-12-05 11:23:48 -0500

    re create advisors categories migration table

---
+++ db/migrate/20151205162253_create_advisors_categories.rb
@@ -0,0 +1,8 @@
+class CreateAdvisorsCategories < ActiveRecord::Migration
+  def change
+    create_table :advisors_categories do |t|
+      t.references :advisor, index: true, foreign_key: true
+      t.references :category, index: true, foreign_key: true
+    end
+  end
+end
