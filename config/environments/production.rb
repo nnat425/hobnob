b5853dfd67decf5f1da44fe20b426e9ae168aab6
Navraj Nat <nnat425@gmail.com>
2015-12-08 17:43:11 -0500

    Change name of AWS variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ end
     :storage => :s3,
     :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => {
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :access_key_id => ENV['AWSAccessKeyId'],
+      :secret_access_key => ENV['AWSSecretKey']
       }
     }
 
