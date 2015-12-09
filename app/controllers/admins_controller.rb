172dc9e6823ec711070a83d1e1f7c39a05796e74
Navraj Nat <nnat425@gmail.com>
2015-12-09 15:00:08 -0500

    Add changes to admin controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,15 +5,22 @@ class AdminsController < ApplicationController
   end
 
   def create #Create Admin session
+    binding.pry
     admin = Admin.find_by(username: session[:username])
-    if admin.try(:authenticate, session_params[:password])
-     session[:user_or_advisor_id] = admin.id
+    if admin.try(:authenticate, admin_session_params[:password])
+     session[:admin_id] = admin.id
      session[:class_type] = admin.class.name
      redirect_to root_path
    else
     flash[:login_fail] = "Please input the correct username/password!"
     redirect_to admin_login_path
   end
+end
+
+private
+
+def admin_session_params
+  params.require(:session).permit(:username, :password)
 end
 
 
