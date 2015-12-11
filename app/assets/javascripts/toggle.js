dea89e25b91cbf48f2e7c21ee61d9743b0396bf4
Jenny Lee <leejenny0403@gmail.com>
2015-12-10 19:12:21 -0500

    slide toggle when hovering over div boxes on home page

--- app/assets/javascripts/toggle.js
+++ app/assets/javascripts/toggle.js
@@ -16,12 +16,29 @@ $(document).ready(function(){
     $(".learn-toggle").fadeToggle();
   });
 
-  $(".home-box-info").hide();
+  $(".home-box-toggle").hide();
 
   $( ".home-subheader").hover(function() {
-    var id = $(this).attr('id');
-    $("#info" + id).fadeToggle();
-  });
+      var id = $(this).attr('id');
+      $(this).css('background-color','#F0F8FF');
+      $(this).css('border','1px solid #2996cc');
+      $("#home-icon-" + id).css("background-color", "#fff");
+      $("#home-icon-" + id).css("border", "1px solid #fff");
+      $("#home-icon-" + id + " > h1").css("color", "#2996cc");
+      $("#info-" + id).hide();
+      $( "#info" + id).slideDown();
+    },
+    function() {
+      var id = $(this).attr('id');
+      $(this).css('background-color','#fff');
+      $(this).css('border','1px solid #C0C0C0');
+      $("#home-icon-" + id).css("background-color", "#2996cc");
+      $("#home-icon-" + id + " > h1").css("color", "#fff");
+      $("#info" + id).hide();
+      $( "#info-" + id).slideDown();
+
+    });
+
 
 
 
