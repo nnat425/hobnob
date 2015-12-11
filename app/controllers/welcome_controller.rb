1680be7092869f4ad539a4cdcec4a189325b8de5
Jenny Lee <leejenny0403@gmail.com>
2015-12-10 23:03:58 -0500

    add comments migration table

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @comments = Comment.all
   end
 
   def about
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209185544) do
+ActiveRecord::Schema.define(version: 20151211030335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,14 @@ ActiveRecord::Schema.define(version: 20151209185544) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author",     null: false
+    t.string   "body",       null: false
+    t.string   "subtext"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
