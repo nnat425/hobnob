7894a3fbcaab9546533087e48ff317df4b03d777
Navraj Nat <nnat425@gmail.com>
2015-12-13 17:17:03 -0500

    Add cart redirects to correct page when not logged in

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,15 +5,19 @@ class CartsController < ApplicationController
   end
 
   def update #Adds an item to the current cart
-    potential_appointment = PotentialAppointment.find_by(id:params[:advisor][:appointment_id])
-    if current_cart.potential_appointments.include?(potential_appointment)
-      flash[:already_in_cart] = "This appointment is already in your cart"
-      redirect_to advisor_path
+    if current_user
+      potential_appointment = PotentialAppointment.find_by(id:params[:advisor][:appointment_id])
+      if current_cart.potential_appointments.include?(potential_appointment)
+        flash[:already_in_cart] = "This appointment is already in your cart"
+        redirect_to advisor_path
+      else
+        current_cart.potential_appointments.push(potential_appointment)
+        current_cart.update(checked_out?: false)
+        flash[:added_to_cart] = "Successfully Added To Cart!"
+        redirect_to advisor_path
+      end
     else
-      current_cart.potential_appointments.push(potential_appointment)
-      current_cart.update(checked_out?: false)
-      flash[:added_to_cart] = "Successfully Added To Cart!"
-      redirect_to advisor_path
+      redirect_to new_user_path
     end
   end
 
