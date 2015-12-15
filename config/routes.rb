794a4fedb31cfe5a0b587d57c6f12aadf9211927
Jenny Lee <leejenny0403@gmail.com>
2015-12-15 13:19:26 -0500

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,6 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
-  get '/categories/:id/' => 'categories#show'
-
-  get '/network_overview' => 'welcome#network_overview'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
