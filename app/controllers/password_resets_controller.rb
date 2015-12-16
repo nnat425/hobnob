3ee9ef6a7b564e4aa3f262fcd2f66e1e842f96db
Navraj Nat <nnat425@gmail.com>
2015-12-16 13:30:31 -0500

    Add create method in password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,12 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(email: params[:email])
+    user.send_password_reset if user
+    redirect_to root_url, :notice => "Email sent with password reset instructions."
+  end
+
   def edit
   end
 end
