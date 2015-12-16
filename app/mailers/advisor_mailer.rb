8cf6543570381653eed6b06deb53846bb0a7dffe
Navraj Nat <nnat425@gmail.com>
2015-12-16 15:28:26 -0500

    Add method in advisor mailer

--- app/mailers/advisor_mailer.rb
+++ app/mailers/advisor_mailer.rb
@@ -6,4 +6,9 @@ class AdvisorMailer < ApplicationMailer
     @advisor = Advisor.find_by(id: appointment.advisor_id)
     mail(to: @advisor.email, subject: 'Hobnob:An appointment has been booked!')
   end
+
+  def password_reset(advisor)
+    @advisor = advisor
+    mail :to => advisor.email, :subject => "Password Reset"
+  end
 end
