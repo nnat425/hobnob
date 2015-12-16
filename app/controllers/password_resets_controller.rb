58403f9cfff0ea2ce45d4997ae14cb2c72601b4b
Navraj Nat <nnat425@gmail.com>
2015-12-16 14:49:34 -0500

    Password reset for user

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -18,11 +18,17 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset has expired."
-    elsif @user.update_attributes(params[:user])
+    elsif @user.update_attributes(password:params[:user][:password],password_confirmation:params[:user][:password_confirmation],email_confirmation: @user.email)
       redirect_to root_url, :notice => "Password has been reset!"
     else
       render :edit
     end
   end
+
+  private
+
+  # def password_params
+  #   params.require(:user).permit(:password,:password_confirmation)
+  # end
 
 end
