a12e38128d840e687c9bc45bd865519c1d388e49
Navraj Nat <nnat425@gmail.com>
2015-12-16 15:02:54 -0500

    Add flash pw reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,7 +6,8 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     user.send_password_reset if user
-    redirect_to root_url, :notice => "Email sent with password reset instructions."
+    flash[:pw_reset] = "Email sent with password reset instructions."
+    redirect_to root_url
   end
 
   def edit
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,7 +56,7 @@ end
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  config.action_mailer.default_url_options = { :host => "localhost" }
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
