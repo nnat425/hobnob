4683273602f99c429d45f303f1f09fe93fe777e3
Navraj Nat <nnat425@gmail.com>
2015-12-19 14:44:34 -0500

    Change application and user default mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "info@hobnob.global"
+  default from: "nnat425@gmail.com"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'info@global.com'
+  default from: 'nnat425@gmail.com'
 
   def confirmation_email(user, appointment)
     @user = user
