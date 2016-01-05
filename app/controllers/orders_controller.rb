2376943df3d43413195466c0d87e944c198aab36
Navraj Nat <nnat425@gmail.com>
2016-01-05 16:54:22 -0500

    Add controller logic

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,23 +16,37 @@ class OrdersController < ApplicationController
       #   AdvisorMailer.confirmation_email(current_user,appointment).deliver_now
       #   AdminMailer.confirmation_email(current_user,appointment).deliver_now
       # end
-        current_user.carts.create
-        render template: "success"
-      else
-        render template: "failure"
-      end
+      current_user.carts.create
+      render template: "success"
     else
-      render :action => 'new'
+      render template: "failure"
+    end
+  else
+    render :action => 'new'
+  end
+end
+
+def show
+  @order = Order.find_by(id: params[:id])
+  @advisors = []
+  @order.cart.potential_appointments.each do |pot_appointment|
+    @advisors.push(Advisor.find_by(id: pot_appointment.advisor_id))
+  end
+  respond_to do |format|
+    format.html
+    format.pdf do
+      render :pdf => 'file_name',
+      :template => 'forms/registration_show.pdf.erb',
+      :layout => 'pdf.html.erb',
+      :show_as_html => params[:debug].present?
     end
   end
+end
 
-  def show
-  end
+private
 
-  private
-
-  def order_params
-    params.require(:order).permit(:first_name,:last_name,:card_type,:card_number,:card_verification,:card_expires_on)
-  end
+def order_params
+  params.require(:order).permit(:first_name,:last_name,:card_type,:card_number,:card_verification,:card_expires_on)
+end
 
 end
