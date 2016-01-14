091dde1b311f22401f6312ec9d703c775859fc50
Navraj Nat <nnat425@gmail.com>
2016-01-14 15:21:31 -0500

    Add template for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,9 @@ class User < ActiveRecord::Base
   validates_confirmation_of :email, :message => "should match email"
   validates_presence_of :email_confirmation
 
-  before_create :create_activation_digest
 
+  attr_accessor :activation_token
+  before_create :create_activation_digest
 
   has_secure_password
 
@@ -43,6 +44,31 @@ class User < ActiveRecord::Base
     save!
     UserMailer.password_reset(self).deliver
   end
+
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  private
+
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
 
 
 end
