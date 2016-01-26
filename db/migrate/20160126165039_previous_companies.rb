13bb8a7eff15dd8fbbe3ac26b2671deac819a27c
Jenny Lee <leejenny0403@gmail.com>
2016-01-26 12:27:44 -0500

    create table previous companies with reference to advisor migration

--- db/migrate/20160126165039_previous_companies.rb
+++ db/migrate/20160126165039_previous_companies.rb
@@ -1,4 +1,10 @@
 class PreviousCompanies < ActiveRecord::Migration
   def change
+    create_table :previous_companies do |t|
+      t.references :advisor
+      t.string :name
+      t.string :job_title
+      t.string :roles
+    end
   end
 end
