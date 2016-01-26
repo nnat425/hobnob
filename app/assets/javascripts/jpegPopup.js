00d3bc3af1e45d4eb9ef0ad9b10bbe8d55dd596d
Navraj Nat <nnat425@gmail.com>
2016-01-26 13:16:44 -0500

    Popup is working effectively

--- app/assets/javascripts/jpegPopup.js
+++ app/assets/javascripts/jpegPopup.js
@@ -5,11 +5,24 @@ $(document).ready(function(){
     $('.b-modal').show();
     console.log("WTF");
   });
-
-
   $('#element_to_pop_up').click(function(){
     $('#element_to_pop_up').hide();
     $('.b-modal').hide();
   });
+
+  $('#locations').on("click",function(){
+    $('#location_to_pop_up').show();
+    $('.b-modal').show();
+    console.log("WTF");
+  });
+  $('#location_to_pop_up').click(function(){
+    $('#location_to_pop_up').hide();
+    $('.b-modal').hide();
+  });
+
+
+
+
+
 
 });
