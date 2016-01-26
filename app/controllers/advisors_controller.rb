a35aa6da45a7cb42dad0ce8b7cc51aab41723ca9
Jenny Lee <leejenny0403@gmail.com>
2016-01-25 23:52:16 -0500

    readd flash thank you alert box

--- app/controllers/advisors_controller.rb
+++ app/controllers/advisors_controller.rb
@@ -48,7 +48,7 @@ class AdvisorsController < ApplicationController
       session[:user_or_advisor_id] = advisor.id
       session[:class_type] = advisor.class.name
       AdvisorMailer.email_verification(advisor).deliver_now
-      flash[:thank_you] = "Thank you! Please check your email to verify your account."
+      flash[:thank_you] = "Thank you!"
       redirect_to edit_advisor_path(advisor)
     else
       flash[:errors] = advisor.errors.full_messages
