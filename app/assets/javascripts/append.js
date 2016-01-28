6c2c80e81d11ec0b37b4c7147edc81a38f361eef
Jenny Lee <leejenny0403@gmail.com>
2016-01-27 23:33:39 -0500

    add ajax to add newly created previous company to edit page

--- app/assets/javascripts/append.js
+++ app/assets/javascripts/append.js
@@ -1,22 +1,33 @@
 $(document).ready(function(){
   $("#edit-panel").on("click", "#add-previous-company-btn", function(event) {
     event.preventDefault();
-    console.log("prevented default");
 
     $.ajax({
       url: "/previous_companies/new",
       method: "get"
     }).done(function(data) {
-      console.log(data);
       $("#previous_companies_holder").append(data);
     }).fail(function() {
       console.log("did not ajax");
     });
 
- });
+  });
 
-  $("#edit-previous-company-form-holder").on("submit", "#add-company-btn", function(event) {
+  $("#edit-panel").on("click", "#add-company-btn", function(event) {
     event.preventDefault();
-  }
+    console.log("prevented default");
+    var info = $(".new_previous_company").serialize();
+    console.log(info);
+    $.ajax({
+      url:"/previous_companies",
+      method: "post",
+      data: info
+    }).done(function(data) {
+      $("#previous_companies_holder").append(data);
+    }).fail(function(data) {
+      console.log(data);
+      console.log("did not ajax");
+    })
+  });
 
 });
--- app/controllers/previous_companies_controller.rb
+++ app/controllers/previous_companies_controller.rb
@@ -1,8 +1,33 @@
 class PreviousCompaniesController < ApplicationController
+
+  def index
+    redirect_to edit_advisor_path(current_advisor)
+  end
+
   def new
     @previous_company = PreviousCompany.new
     if request.xhr?
       render partial: 'new_previous_company_form'
     end
+  end
+
+  def create
+    @advisor = current_advisor
+    @new_company = current_advisor.previous_companies.create(previous_company_params)
+    if request.xhr?
+      render partial: "new_previous_company"
+    else
+      render :"advisors/edit"
+    end
+    # respond_to do |fmt|
+    #   fmt.html { render :"advisors/edit" }
+    #   fmt.json { render partial: "new_previous_company" }
+    # end
+  end
+
+  private
+
+  def previous_company_params
+    params.require(:previous_company).permit(:name, :job_title, :roles)
   end
 end
