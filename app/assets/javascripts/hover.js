9f18b036252b3aeff5672dc2b7e5351f6df35e1c
Navraj Nat <nnat425@gmail.com>
2016-02-02 15:52:09 -0500

    Add phone extension

--- app/assets/javascripts/hover.js
+++ app/assets/javascripts/hover.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-  $('#investment_banking_info_pop_network').click(function(){
-    $('#investment_banking_popup_network').slideToggle();
+  $('#investment_banking_info_pop_network_phone').click(function(){
+    $('#investment_banking_popup_network_phone').slideToggle();
   });
   $('#asset_management_info_pop_network_phone').click(function(){
     $('#asset_management_popup_network_phone').slideToggle();
@@ -11,6 +11,9 @@ $(document).ready(function(){
   });
   $('#other_finance_info_pop_network_phone').click(function(){
     $('#other_finance_popup_network_phone').slideToggle();
+  });
+  $('#resume_interview_info_pop_network_phone').click(function(){
+    $('#resume_interview_popup_network_phone').slideToggle();
   });
 });
 
