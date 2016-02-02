62c3fa36b7418fe0ee98dfb86890cb65b9dbceff
Navraj Nat <nnat425@gmail.com>
2016-02-02 15:30:06 -0500

    Asset management for phone displaying correctly

--- app/assets/javascripts/hover.js
+++ app/assets/javascripts/hover.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 
   $('#investment_banking_info_pop_network').click(function(){
     $('#investment_banking_popup_network').slideToggle();
-
+  });
+  $('#asset_management_info_pop_network_phone').click(function(){
+    $('#asset_management_popup_network_phone').slideToggle();
   });
 });
 
