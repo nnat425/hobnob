4806da09cb4153c9d7f8aab276aba9d9d32ed5c2
Jenny Lee <leejenny0403@gmail.com>
2016-02-04 13:03:15 -0500

    move all toggle to toggle file and remove hover file

--- app/assets/javascripts/toggle.js
+++ app/assets/javascripts/toggle.js
@@ -104,5 +104,28 @@ $(document).ready(function(){
     $("#categories-toggle").slideToggle();
   });
 
+  $('#investment_banking_info_pop_network_phone').on("click",function(event){
+  event.preventDefault();
+  $('#investment_banking_popup_network_phone').slideToggle();
+});
+$('#asset_management_info_pop_network_phone').on("click", function(event){
+  event.preventDefault();
+  $('#asset_management_popup_network_phone').slideToggle();
+});
+$('#corporate_finance_info_pop_network_phone').on("click", function(event){
+  event.preventDefault();
+  $('#corporate_finance_popup_network_phone').slideToggle();
+});
+$('#other_finance_info_pop_network_phone').on("click", function(event){
+  event.preventDefault();
+  $('#other_finance_popup_network_phone').slideToggle();
+});
+$('#resume_interview_info_pop_network_phone').on("click", function(event){
+  event.preventDefault();
+  $('#resume_interview_popup_network_phone').slideToggle();
+});
+
+
+
 });
 
