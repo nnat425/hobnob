714322c6bbdc49114d7d89f0fd4ddd52b57b9999
Jenny Lee <leejenny0403@gmail.com>
2016-02-04 12:56:47 -0500

    remove popup for desktop

--- app/assets/javascripts/hover.js
+++ app/assets/javascripts/hover.js
@@ -17,47 +17,6 @@ $('#resume_interview_info_pop_network_phone').click(function(){
   $('#resume_interview_popup_network_phone').slideToggle();
 });
 
-//For desktop view
-$("#investment_banking_info_pop_network" ).hover(
-  function() {
-    $('#investment_banking_popup_network').fadeIn(500);
-  }, function() {
-    $('#investment_banking_popup_network').fadeOut(500);
-  }
-  );
-
-$("#asset_management_info_pop_network" ).hover(
-  function() {
-    $('#asset_management_popup_network').fadeIn(500);
-  }, function() {
-    $('#asset_management_popup_network').fadeOut(500);
-  }
-  );
-
-$("#corporate_finance_info_pop_network" ).hover(
-  function() {
-    $('#corporate_finance_popup_network').fadeIn(500);
-  }, function() {
-    $('#corporate_finance_popup_network').fadeOut(500);
-  }
-  );
-
-$("#other_finance_info_pop_network" ).hover(
-  function() {
-    $('#other_finance_popup_network').fadeIn(500);
-  }, function() {
-    $('#other_finance_popup_network').fadeOut(500);
-  }
-  );
-
-$("#resume_interview_info_pop_network" ).hover(
-  function() {
-    $('#resume_interview_popup_network').fadeIn(500);
-  }, function() {
-    $('#resume_interview_popup_network').fadeOut(500);
-  }
-  );
-
 
 
 });
