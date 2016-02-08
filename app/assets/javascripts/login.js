3d90359c26efe481c4bad91515c5c3543e184a8a
Jenny Lee <leejenny0403@gmail.com>
2016-02-08 14:43:43 -0500

    login popup shows up on network page

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,1 +1,8 @@
-login.js
+$(document).ready(function(){
+  $("#advisors-list").on("click", "#must-login", function(event) {
+    event.preventDefault();
+
+    $("#login-pop").bPopup();
+  });
+
+});
