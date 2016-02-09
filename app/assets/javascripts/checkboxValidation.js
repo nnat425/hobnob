1baefc059870b8fd59dfa3a77694ca6a41ec817a
Jenny Lee <leejenny0403@gmail.com>
2016-02-09 15:44:01 -0500

    remove checkbox validator for advisor edit

--- app/assets/javascripts/checkboxValidation.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#edit_signup").on('submit',function(event) {
-   if(!$(('.category-checkbox')).is(':checked') == true) {
-    event.preventDefault();
-    alert("You must pick at least 1 area of experience")
-  }
-})
-});
